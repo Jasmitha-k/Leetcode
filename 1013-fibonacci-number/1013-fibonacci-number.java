b25ac508ec01aabd9fc14eac7a5badaa2626be22
Jasmitha Kaja <jasmithakaja@gmail.com>
2025-02-12 11:43:20 +0530

    Time: 0 ms (100%), Space: 40.4 MB (63%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public  int fib_number(int n){
+        if (n <= 1) return n;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+    public int fib(int n) {
+        Solution sol = new Solution();
+        return(sol.fib_number(n));
+        
+    }
+}
